68f623a99dcb4cc5259c1a57cb98fe7e658fa574
Batı Gökovalı <batigokovali@icloud.com>
2023-01-17 18:31:40 +0300

    carousel

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const getImages = (query) => {
 .then((rawImages)=> {
     return rawImages.json()
 }).then((jsonImages)=> {
-    console.log(jsonImages.photos)
     renderImages(jsonImages.photos)
     replace9mins(jsonImages.photos)
 }).catch(err => console.log(err))
 }
 
+const getForest = () => {
+    fetch(`https://api.pexels.com/v1/search?query=forest`, {method: "GET",
+                headers: {Authorization:"563492ad6f91700001000001b07d2d6695bb4807990af72dd4caaa46"}})
+.then((rawImages)=> {
+    return rawImages.json()
+}).then((jsonImages)=> {
+    renderCarousel(jsonImages.photos)
+}).catch(err => console.log(err))
+}
 
 const renderImages = (fetchedImages) => {
     let parent = document.querySelectorAll(".card-img-top")
@@ -45,5 +53,13 @@ replace9mins()
 
 function searchImages () {
     let userSearch = document.getElementById("search-field").value
+    getImages(userSearch)
 }
 
+const renderCarousel = (forestImages) => {
+    let parent = document.querySelectorAll(".d-block.w-100")
+    for (let i = 0; i< parent.length;i++)
+    {
+        parent[i].setAttribute("src", `${forestImages[i].src.original}`)        
+    }
+}
